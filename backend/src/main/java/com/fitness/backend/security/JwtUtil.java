2a2be755d11ee1d71ca1ac3476d5381d044fe3e0
hhajibadri <hhajibadri@ucsd.edu>
2026-02-02 01:11:47 -0800

    Wip

--- backend/src/main/java/com/fitness/backend/security/JwtUtil.java
+++ backend/src/main/java/com/fitness/backend/security/JwtUtil.java
@@ -13,7 +13,12 @@ public class JwtUtil {
     private final long EXPIRATION_MS = 1000 * 60 * 60 * 10; // 10 hours
 
     public String generateToken(String email) {
-        return Jwts.builder();
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MS))
+                .signWith(SignatuteAlgorithm.HS256, SECRET_KEY)
+                .compact();
     }
 
 }
